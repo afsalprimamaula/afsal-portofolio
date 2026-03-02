533b0646211690091a8253aac27a4f655f4f5a10
afsalprimamaula <afsalprimamaulaaa@gmail.com>
2026-03-02 20:27:01 +0700

    website done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import { ReactNode } from "react"; 
-import "@/app/globals.css";
+import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import BottomNav from "@/components/layout/BottomNav";
 import Footer from "@/components/layout/Footer"; // Import Footer
